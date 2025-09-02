e4fbd962d2b83579de63455e2c2ff2a814770861
NormalMood <littlebigdiamond@gmail.com>
2025-09-02 16:25:28 +0300

    feat: Login page

--- routes/routes.php
+++ routes/routes.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Controllers\LoginController;
 use App\Controllers\PostController;
 use App\Controllers\RegisterController;
 use App\Controllers\SettingsController;
@@ -8,5 +9,6 @@ use App\Kernel\Router\Route;
 return [
     Route::get(uri: '/', action: [PostController::class, 'index']),
     Route::get(uri: '/settings', action: [SettingsController::class, 'index']),
-    Route::get('/signup', [RegisterController::class, 'index'])
+    Route::get('/signup', [RegisterController::class, 'index']),
+    Route::get(uri: '/signin', action: [LoginController::class, 'index'])
 ];
