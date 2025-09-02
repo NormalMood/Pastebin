5418b0035329f8ec1e0703b2874c81827162f2fb
NormalMood <littlebigdiamond@gmail.com>
2025-09-02 13:12:09 +0300

    feat: class Router

---
+++ kernel/Router/Router.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Kernel\Router;
+
+class Router implements RouterInterface
+{
+    private array $routes = [
+        GET => [],
+        POST => []
+    ];
+
+    public function __construct()
+    {
+        $this->initRoutes();
+    }
+
+    public function dispatch(string $uri, string $method): void
+    {
+        $route = $this->findRoute($uri, $method);
+        if (!$route) {
+            $this->notFound();
+        }
+        if (is_array($route->getAction())) {
+            [$controller, $action] = $route->getAction();
+            /**
+             * @var \App\Kernel\Controller\Controller $controller
+             */
+            $controller = new $controller();
+            call_user_func(callback: [$controller, $action]);
+        } else {
+            call_user_func(callback: $route->getAction());
+        }
+    }
+
+    private function findRoute(string $uri, string $method): Route|false
+    {
+        if (!isset($this->routes[$method][$uri])) {
+            return false;
+        }
+        return $this->routes[$method][$uri];
+    }
+
+    private function notFound(): void
+    {
+        echo '404 | Not Found';
+        exit;
+    }
+
+    private function initRoutes(): void
+    {
+        $routes = $this->getRoutes();
+        foreach ($routes as $route) {
+            $this->routes[$route->getMethod()][$route->getUri()] = $route;
+        }
+    }
+
+    /**
+     * @return Route[]
+     */
+    private function getRoutes(): array
+    {
+        return require_once APP_PATH . '/routes/routes.php';
+    }
+}
