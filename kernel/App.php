cf158aa63ca9f684e90575e3a7ab415150594f40
NormalMood <littlebigdiamond@gmail.com>
2025-09-02 13:18:28 +0300

    fix: added constants requiring in index.php

--- kernel/App.php
+++ kernel/App.php
@@ -2,10 +2,16 @@
 
 namespace App\Kernel;
 
+use App\Kernel\Router\Router;
+
 class App
 {
     public function run(): void
     {
-        echo 'run app';
+        $router = new Router();
+        $router->dispatch(
+            uri: $_SERVER['REQUEST_URI'],
+            method: $_SERVER['REQUEST_METHOD']
+        );
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 define('APP_PATH', dirname(__DIR__));
+require_once APP_PATH . '/kernel/constants/constants.php';
 
 require_once APP_PATH . '/vendor/autoload.php';
 
