4cd44e0c980a29181b1c385b610ce2573136609f
NormalMood <littlebigdiamond@gmail.com>
2025-09-02 16:21:56 +0300

    feat: Registration page

--- routes/routes.php
+++ routes/routes.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Controllers\PostController;
+use App\Controllers\RegisterController;
 use App\Controllers\SettingsController;
 use App\Kernel\Router\Route;
 
 return [
     Route::get(uri: '/', action: [PostController::class, 'index']),
-    Route::get(uri: '/settings', action: [SettingsController::class, 'index'])
+    Route::get(uri: '/settings', action: [SettingsController::class, 'index']),
+    Route::get('/signup', [RegisterController::class, 'index'])
 ];
