d773ec14432e919081bc4b9b3910ed0d2284030d
NormalMood <littlebigdiamond@gmail.com>
2025-09-02 18:27:06 +0300

    feat: database config

---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'driver' => 'pgsql',
+    'host' => 'localhost',
+    'port' => 5435,
+    'database' => 'pastebin',
+    'username' => 'pastebin_app',
+    'password' => 'vU1kAn_qZ2',
+    'charset' => 'UTF8'
+];
