e8b2cf9679094a934f28e4381212966e9ea46802
NormalMood <littlebigdiamond@gmail.com>
2025-09-02 12:37:04 +0300

    feat: class PostController

---
+++ src/Controllers/PostController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Kernel\Controller\Controller;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        echo 'post page';
+    }
+}
