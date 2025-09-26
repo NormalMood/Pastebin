2716aad746ceb4a727fbfa336a13df3f326be9b2
NormalMood <littlebigdiamond@gmail.com>
2025-09-26 10:13:08 +0300

    fix: href for styles

--- views/components/start.php
+++ views/components/start.php
@@ -38,8 +38,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.19/codemirror.min.css" />
-    <link href="css/styles.css" rel="stylesheet" />
-    <link href="css/normalize.css" rel="stylesheet" />
+    <link href="/css/styles.css" rel="stylesheet" />
+    <link href="/css/normalize.css" rel="stylesheet" />
 
 
     <style nonce="<?php echo $nonce; ?>">
