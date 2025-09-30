3ba8b52ab1a6d919822956fd15c59c7fa721be46
NormalMood <littlebigdiamond@gmail.com>
2025-09-30 18:42:09 +0300

    fix: copy post by click in browsers that don't support navigator.clipboard

--- public/scripts/copy_post.js
+++ public/scripts/copy_post.js
@@ -2,17 +2,45 @@ const successCopyElement = document.querySelector('.post__success-copy');
 const copyButton = document.querySelector('.post__copy-button button');
 
 if ((successCopyElement) !== null && (copyButton !== null)) {
+    const showSuccessCopyElement = () => {
+        successCopyElement.classList.remove('post__success-copy_hidden');
+        setTimeout(() => {
+            successCopyElement.classList.add('post__success-copy_hidden');
+        }, 2000);
+    }
+
+    const copyText = (text) => {
+        const hiddenTextArea = document.createElement('textarea');
+        hiddenTextArea.value(text);
+        hiddenTextArea.classList.add('text_hidden');
+        document.body.appendChild(hiddenTextArea);
+        hiddenTextArea.focus();
+        hiddenTextArea.select();
+        try {
+            const successful = document.execCommand('copy');
+            if (!successful) {
+                alert('Ошибка копирования');
+            } else {
+                document.body.removeChild(hiddenTextArea);
+                showSuccessCopyElement();
+            }
+        } catch (error) {
+            alert('Ошибка копирования: ' + error);
+        }
+    }
+
     const postText = document.getElementById('post-text');
     copyButton.addEventListener('click', () => {
-        navigator.clipboard.writeText(postText.textContent)
-            .then(() => {
-                successCopyElement.classList.remove('post__success-copy_hidden');
-                setTimeout(() => {
-                    successCopyElement.classList.add('post__success-copy_hidden');
-                }, 2000);
-            })
-            .catch(error => {
-                alert('Ошибка при копировании: ' + error);
-            });
+        if (navigator.clipboard && navigator.clipboard.writeText) {
+            navigator.clipboard.writeText(postText.textContent)
+                .then(() => {
+                    showSuccessCopyElement();
+                })
+                .catch(error => {
+                    copyText(postText.textContent);
+                });
+        } else {
+            copyText(postText.textContent);
+        }
     });
 }
